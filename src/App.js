77e664bd55a1fff1985d583fb2ebd6d2942fce8e
Anderson <anderson.mc@hotmail.com>
2022-12-01 19:08:14 -0300

    first commands

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/header";
+import { Global } from "./styles/global";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Global />
+        </>
+    );
+}
+
+export default App;
